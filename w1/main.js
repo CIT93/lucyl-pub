11e390614ebf6c500e7a5ff496d229c3e6e2289c
FCCStudent2025 <llarange1@my.scccd.edu>
2025-01-24 19:40:46 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log("Week 1 -Hello Word");
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Lucy";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNOW () {
+    if(myVarType === "number") {
+        console.log(`will 11 this one run?`);
+    } else {
+        console.log(`will 13 this one run?`);
+    }
+}
+
+
+runNOW();
+runNOW();
