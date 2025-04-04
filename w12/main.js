86bcf5a9c2e455664de236403fc4a5aea0e3a598
FCCStudent2025 <llarange1@my.scccd.edu>
2025-04-04 14:19:58 +0000

    code with solution

--- w12/main.js
+++ w12/main.js
@@ -1,27 +1,49 @@
 const FORM = document.getElementById("form");
 const output = document.getElementById("output");
 
-function getFit() {
-    const heading = document.createElement("p")
-    heading.textContent = "Submit to start!"
-    output.appendChild(heading)
-
-    FORM.addEventListener('submit', (e) => {
-        e.preventDefault();  
-
-        const exerciseType = document.getElementById('exercise').value;  
-        const numberReps = document.getElementById('reps').value;
-        const timeSet = document.getElementById('time').value;
-
-        const outputMessage = document.createElement('p');
-        outputMessage.textContent = `Start ${exerciseType} < > Goal rep is ${numberReps}`;
-        output.appendChild(outputMessage);
-
-        setTimeout(() => {
-           const timeout = document.createElement('p')
-           timeout.textContent = `Stop doing ${exerciseType}`
-           output.appendChild(timeout);
-        }, 3000);
-    });
+function updateDOM(message, el) {
+  const newEL = document.createElement(el);
+  newEL.textContent = message;
+  output.appendChild(newEL);
 }
-getFit()
+
+const startWorkout = (type, reps, time, fn) => {
+  fn(`Start ${type} <> Goal reps is ${reps} <> complete in ${time} min!`, "p");
+  setTimeout(() => {
+    fn(`Stop ${type}`, "h1");
+  }, time * 1000);
+};
+
+FORM.addEventListener("submit", e => {
+  e.preventDefault();
+  const type = e.target.type.value;
+  const reps = parseFloat(e.target.reps.value);
+  const time = parseFloat(e.target.time.value);
+  startWorkout(type, reps, time, updateDOM);
+  FORM.reset();
+});
+
+// function getFit() {
+//     const heading = document.createElement("p")
+//     heading.textContent = "Submit to start!"
+//     output.appendChild(heading)
+
+//     FORM.addEventListener('submit', (e) => {
+//         e.preventDefault();
+
+//         const exerciseType = document.getElementById('exercise').value;
+//         const numberReps = document.getElementById('reps').value;
+//         const timeSet = document.getElementById('time').value;
+
+//         const outputMessage = document.createElement('p');
+//         outputMessage.textContent = `Start ${exerciseType} < > Goal rep is ${numberReps}`;
+//         output.appendChild(outputMessage);
+
+//         setTimeout(() => {
+//            const timeout = document.createElement('p')
+//            timeout.textContent = `Stop doing ${exerciseType}`
+//            output.appendChild(timeout);
+//         }, 3000);
+//     });
+// }
+// getFit()
